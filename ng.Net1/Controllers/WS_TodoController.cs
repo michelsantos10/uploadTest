d2a9351c523700a14cb285aba38e8496e0f9ab67
miche <miche@NOTE1>
2016-10-05 22:45:18 -0300

    Add project files.

---
+++ ng.Net1/Controllers/WS_TodoController.cs
@@ -0,0 +1,153 @@
+﻿using ng.Net1.Models;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Identity.Owin;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+using System.Web;
+using System.Diagnostics;
+using System.Web.Http.Cors;
+
+namespace ng.Net1.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class WS_TodoController : ApiController
+    {
+        private DBContext db = new DBContext();
+        //HttpContext httpContext = new HttpContext(new Http
+
+        public RoleManager<IdentityRole> RoleManager { get; private set; }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        [HttpGet]
+        [Authorize]
+        public List<todoItem> GetUserTodoItems()
+        {
+            string userId = Request.GetOwinContext().Authentication.User.Identity.GetUserId();
+
+            var currentUser = UserManager.FindById(userId);
+            return currentUser.todoItems;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public HttpResponseMessage PostTodoItem(TodoItemViewModel item)
+        {
+            var modelStateErrors = ModelState.Values.ToList();
+
+            //HttpContext.Current.Session
+
+            List<string> errors = new List<string>();
+
+            foreach (var s in modelStateErrors)
+                foreach (var e in s.Errors)
+                    if (e.ErrorMessage != null && e.ErrorMessage.Trim() != "")
+                        errors.Add(e.ErrorMessage);
+
+            if (errors.Count == 0)
+            {
+                try
+                {
+                    string userId = Request.GetOwinContext().Authentication.User.Identity.GetUserId();
+
+                    var currentUser = UserManager.FindById(userId);
+                    currentUser.todoItems.Add(new todoItem()
+                    {
+                        completed = false,
+                        task = item.task
+                    });
+
+                    UserManager.Update(currentUser);
+                    return Request.CreateResponse(HttpStatusCode.Accepted);
+                }
+                catch
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError);
+                }
+            }
+            else
+            {
+                return Request.CreateResponse<List<string>>(HttpStatusCode.BadRequest, errors);
+            }
+
+            var user = db.Users.Where(u => u.firstName == "Test").FirstOrDefault();
+        }
+
+        [HttpPost]
+        [Authorize]
+        async public Task<HttpResponseMessage> CompleteTodoItem(int id)
+        {
+            var item = db.todos.Where(t => t.id == id).FirstOrDefault();
+            if (item != null)
+            {
+                item.completed = true;
+                await db.SaveChangesAsync();
+            }
+            return Request.CreateResponse(HttpStatusCode.Accepted);
+        }
+
+        [HttpPost]
+        [Authorize]
+        async public Task<HttpResponseMessage> DeleteTodoItem(int id)
+        {
+            var item = db.todos.Where(t => t.id == id).FirstOrDefault();
+            if (item != null)
+            {
+                db.todos.Remove(item);
+                await db.SaveChangesAsync();
+            }
+            return Request.CreateResponse(HttpStatusCode.Accepted);
+        }
+
+        [HttpPost]
+        //[Authorize]
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                // Read the form data.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                // This illustrates how to get the file names.
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    Trace.WriteLine("Server file path: " + file.LocalFileName);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+
+    }
+}
